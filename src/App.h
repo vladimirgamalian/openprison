c3d3fbb6e232b65e9bdf17aefd71f6c424d642df
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2016-07-23 20:21:30 +0700

    Reorganize App module

--- src/App.h
+++ src/App.h
@@ -1,10 +1,25 @@
 #pragma once
+#include <SDL2pp/SDL2pp.hh>
+#include "World.h"
 
 class App
 {
 public:
+	App();
 	void execute();
 
 private:
+	void init();
+	void mainLoop();
+	void update();
 
+	SDL2pp::SDL sdl;
+	SDL2pp::Window window;
+	SDL2pp::Renderer renderer;
+	World world;
+
+	bool exitFlag = false;
+	float worldScale;
+	float shiftX;
+	float shiftY;
 };
